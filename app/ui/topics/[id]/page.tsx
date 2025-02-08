ae486043a504fdbace891b912ab43b835e9565e7
Cody Walenciak <codywalenciak@gmail.com>
2025-02-08 13:10:35 -0600

    Give each question a link to heir respective answers page

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -2,6 +2,7 @@ import { AskQuestion } from "@/components/AskQuestion";
 import { Question } from "@/components/Question";
 import { fetchQuestions, fetchTopic } from "@/lib/data";
 import { HashtagIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
@@ -19,12 +20,14 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
             </h1>
             <AskQuestion topic={topic.id} />
             {questions.map((question) => (
-                <Question
-                    key={question.id}
-                    id={question.id}
-                    text={question.title}
-                    votes={question.votes}
-                />
+                <Link href={`/ui/questions/${question.id}`}>
+                    <Question
+                        key={question.id}
+                        id={question.id}
+                        text={question.title}
+                        votes={question.votes}
+                    />
+                </Link>
             ))}
         </div>
     );
