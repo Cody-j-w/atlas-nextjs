8aba661292140b983a27d0e2dd286a2f6ca4e87e
Cody Walenciak <codywalenciak@gmail.com>
2025-02-12 11:30:21 -0600

    Style adjustments

--- components/LoggedInUser.tsx
+++ components/LoggedInUser.tsx
@@ -13,8 +13,8 @@ export default async function LoggedInUser() {
     }
 
     return (
-        <div className="flex h-[48px] w-full grow items-center justify-center gap-2">
-            <img className="w-10 rounded-full" src={image} alt="User Avatar" />
+        <div className="flex max-h-[48px] w-full grow items-center justify-start text-sm gap-1">
+            <img className="w-8 ms-2 rounded-full" src={image} alt="User Avatar" />
             <p>{session.user.name}</p>
         </div>
     )
